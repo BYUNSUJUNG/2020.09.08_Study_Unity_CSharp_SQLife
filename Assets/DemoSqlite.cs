eb9dc2c6331aa252ed034ffd978632fb705d4394
BYUNSUJUNG <susususu07@naver.com>
2020-10-27 23:53:41 +0900

    git init 2020-10-27

---
+++ Assets/DemoSqlite.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class DemoSqlite : MonoBehaviour
+{
+    public string m_DatabaseFileName = "NumData.db";
+    public string m_TableName = "NumDataTable";
+    private SqliteDatabaseAccess m_SqliteDatabaseAccess;
+
+    void Start()
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath, m_DatabaseFileName);
+        Debug.Log(filePath);
+        m_SqliteDatabaseAccess = new SqliteDatabaseAccess("data source = " + filePath);
+
+        m_SqliteDatabaseAccess.CreateTable("NumDataTable01",
+            new string[] { "wearing", "wearingNum" },
+            new string[] { "text", "int" });
+
+        //m_SqliteDatabaseAccess.InsertInto("TestTable1", new string[] { "'Coderzedro'", "'47'" });
+        //m_SqliteDatabaseAccess.InsertInto("TestTable1", new string[] { "'JD'", "'17'" });
+        //m_SqliteDatabaseAccess.InsertInto("TestTable1", new string[] { "'Tiger'", "'47'" });
+
+        m_SqliteDatabaseAccess.CloseSqlConnection();
+    }
+}
